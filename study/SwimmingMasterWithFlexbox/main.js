0fe921398470522a1e99e6ffe69ac9b7e9e8f197
selcouthonism <37469324+selcouthonism@users.noreply.github.com>
2021-08-24 13:32:08 +0300

    Update main.js

--- study/SwimmingMasterWithFlexbox/main.js
+++ study/SwimmingMasterWithFlexbox/main.js
@@ -6,3 +6,18 @@ function openCloseDropDownMenu(){
 		toggle.className = "topnav";
 	}
 }
+
+var imgIndex = 0;
+carousel();
+
+function carousel() {
+	var i;
+	var x = document.getElementsByClassName("slide");
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";  
+	}
+	imgIndex++;
+	if (imgIndex > x.length) {imgIndex = 1}    
+	x[imgIndex-1].style.display = "block";  
+	setTimeout(carousel, 2000); // Change image every 2 seconds
+}
